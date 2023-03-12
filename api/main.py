f3e3aa15a86953e3fbc627a37ba5356b3fd47052
Manas Sharma <manaskgp2593@kgpian.iitkgp.ac.in>
2023-03-12 09:43:23 +0530

    new project

---
+++ api/main.py
@@ -0,0 +1,59 @@
+# Backend to serve our Potato_Disease_Classifier Model
+
+
+# importing libraries
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+import matplotlib
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+from pydantic import BaseModel
+
+# creating an fastAPI app to launch our model
+app=FastAPI()
+
+# defining the ports and middleware
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# specifying our model and class_names
+MODEL=tf.keras.models.load_model("../Models/1")
+CLASS_NAMES=['EARLY_BLIGHT','LATE_BLIGHT','HEALTHY']
+
+@app.get('/ping')
+async def ping():
+    return 'STATUS:200 WORKING'
+
+# reading input images
+def read_file_as_image(data)->np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post('/predict')
+async def predict(file:UploadFile=File(...)):
+    image=read_file_as_image(await file.read())
+    img_batch=np.expand_dims(image,0)
+    predictions=MODEL.predict(img_batch)
+    predicted_class=CLASS_NAMES[np.argmax(predictions[0])]
+    confidence=np.max(predictions[0])
+    return {
+        'class':predicted_class,
+        'confidence':float(confidence)
+    }
+
+
+
+if __name__=='__main__':
+    uvicorn.run(app,host='localhost',port=8000)
